0ebad1278eef33a17e1c5079f61f7dd8ed83446b
IvanLLF <1148509458@qq.com>
2021-07-15 18:04:00 +0800

    Update api wallet

--- api/wallet.go
+++ api/wallet.go
@@ -14,10 +14,10 @@ func RouterApiServer() *gin.Engine {
 
 	router := gin.Default()
 
-	/* 离线钱包
-	1、生成钱包
-	2、保存钱包地址
-	3、保存钱包私钥
+	/* Offline Wallet
+	1、Generate a wallet
+	2、Save wallet address
+	3、Save wallet private key
 	*/
 	router.GET("/wallet/new", srv.newWallet)
 	router.GET("/wallet/list", srv.walletList)
@@ -25,10 +25,10 @@ func RouterApiServer() *gin.Engine {
 	router.GET("/wallet/import", srv.walletImport)
 	router.GET("/wallet/delete", srv.walletDelete)
 
-	/* 离线签名
-	1、读取消息体
-	2、消息签名
-	3、任意签名
+	/* Off-line signature
+	1、Read message body
+	2、Message signature
+	3、Any signature
 	*/
 	router.GET("/wallet/sig", srv.sig)
 	router.GET("/wallet/sigAny", srv.sigAny)
